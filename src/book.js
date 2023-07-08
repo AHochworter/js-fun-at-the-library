38c70149e5757593ebb98fde126703c03d0b6d79
Ann Hochworter <ann.hochworter@gmail.com>
2023-07-08 09:48:32 -0600

    Include answers for the book.js file and draft answers for library.js

--- src/book.js
+++ src/book.js
@@ -1,12 +1,63 @@
-function createTitle() {
-  
+//  FUNCTION createTitle
+function createTitle(title, modTitle) {
+  return `The ${title}`;
+  // return "The Storm's Awakening";
 }
+//  FUNCTION buildMainCharacter
+function buildMainCharacter(name, age, pronouns) {
+  var character = {
+    name: name,
+    age: age,
+    pronouns: pronouns,
+  };
+  return character;
+}
+//  FUNCTION saveReview
+
+function saveReview(newReview, reviews) {
+  //add reviews to an array(empty to start);
+  //refactor code to check for unique reviews
+  //watch the order of the parameters in theu unit test! Match it!
+  if (reviews.includes(newReview) === false) reviews.push(newReview);
+}
+var reviews = [];
+var newReview = 'An astounding success';
+saveReview(newReview, reviews);
+
+//  FUNCTION calculatePageCount
+function calculatePageCount(title) {
+  //add 20 pages per letter in the title
+  var titleCount = title.length * 20;
+  // console.log(title.length, titleCount);
+  return titleCount;
+  //parameter of a title
+  //count the letters in the title - string length
+  //multiply that count by 20
+}
+//  FUNCTION writeBook
+function writeBook(bookTitle, bookCharacter, genre) {
+  //return a book object
+  //the object key-value pairs link to previous functions
+  var book = {
+    title: bookTitle,
+    mainCharacter: bookCharacter,
+    pageCount: calculatePageCount(bookTitle),
+    genre: genre,
+  };
+  return book;
+}
+//  FUNCTION editBOOK
+function editBook(book) {
+  //decrease the books page count to be 3/4 of what it was
+  book.pageCount = Math.round(book.pageCount * 0.75);
+}
+//I have a question here about solidifying where book.pageCount is coming from?  book object?  I was making this far more comples than it needed to be.
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
-}
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook,
+};
--- src/library.js
+++ src/library.js
@@ -1,8 +1,35 @@
+//  FUNCTION createLibrary
 
+function createLibrary(name) {
+  var library = {
+    name: name,
+    shelves: {
+      fantasy: [],
+      fiction: [],
+      nonFiction: [],
+    },
+  };
+  return library;
+}
+
+//TODO FUNCTION addBook
+function addBook(library, book) {
+  //add a book to the fantasy shelf
+  //add to the shelves array inside the library object
+  //library.shelves.fantasy.push() to index [0]
+  //add to correct shelf automatically - conditional statement?
+  return library.shelves.fantasy.push(book);
+}
+
+//TODO FUNCTION checkoutBook
+function checkoutBook() {}
+
+//TODO FUNCTION takeStock
+function takeStock() {}
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook,
-  // takeStock
+  createLibrary,
+  addBook,
+  checkoutBook,
+  takeStock,
 };
